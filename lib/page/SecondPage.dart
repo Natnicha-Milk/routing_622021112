64e9c4e1081964f2979768c626572a92b5aee948
Natnicha-Milk <622021112@tsu.ac.th>
2022-01-06 22:23:07 +0700

    first commit

--- lib/page/SecondPage.dart
+++ lib/page/SecondPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'thirdpage.dart';
 
 class SecondPage extends StatefulWidget {
@@ -10,17 +10,24 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _stdId = TextEditingController();
+  final TextEditingController _stdName = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('First Page'),
       ),
-      body: Center(
+      body: Form(
+        key: _formKey,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('my First'),
+            const Text('กรอกข้อมูลนิสิต'),
+            StudentID(),
+            Name(),
             ElevatedButton(
                 onPressed: () {
                   Navigator.pop(context);
@@ -28,16 +35,63 @@ class _SecondPageState extends State<SecondPage> {
                 child: const Text('ไปหน้าเเรก')),
             ElevatedButton(
                 onPressed: () {
-                  var route = MaterialPageRoute(
-                    builder: (context) => ThirdPsge(
-                      stdId: '622021112',
-                      stdName: 'milk',
-                    ),
-                  );
-                  Navigator.push(context, route);
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ThirdPsge(
+                            stdId: _stdId.text,
+                            stdName: _stdName.text,
+                          ),
+                        ));
+                  }
                 },
                 child: const Text('ยืนยันเเละส่งข้อมูลไปหน้าที่ 3'))
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget Name() {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: TextFormField(
+        controller: _stdName,
+        validator: (Value) {
+          if (Value!.isEmpty) {
+            return "Please Enter ID";
+          }
+          return null;
+        },
+        decoration: const InputDecoration(
+          labelText: 'Student Name',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(32)), //ใส่ขอบกลม
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget StudentID() {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: TextFormField(
+        controller: _stdId,
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        keyboardType: TextInputType.number,
+        validator: (Value) {
+          if (Value!.isEmpty) {
+            return "Please Enter ID";
+          }
+          return null;
+        },
+        decoration: const InputDecoration(
+          labelText: 'Student ID',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(32)), //ใส่ขอบกลม
+          ),
         ),
       ),
     );
